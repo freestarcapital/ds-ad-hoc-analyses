dce617bfbdc3bd136fd7586a0f2627451f3ac0c1
hughb <hughbarnes@freestar.io>
2025-07-15 18:30:36 +0100

    added fs_ad_product uplift

---
+++ DAS_uplift/main_fs_ad_product_uplift.py
@@ -0,0 +1,67 @@
+import dateutil.utils
+import pandas as pd
+import matplotlib.pyplot as plt
+from google.cloud import bigquery
+import configparser
+from google.cloud import bigquery_storage
+import os, sys
+import numpy as np
+import datetime
+import pickle
+import plotly.express as px
+import kaleido
+from scipy.stats import linregress
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+config_path = '../config.ini'
+config = configparser.ConfigParser()
+config.read(config_path)
+
+project_id = "freestar-157323"
+client = bigquery.Client(project=project_id)
+bqstorageclient = bigquery_storage.BigQueryReadClient()
+
+
+def get_bq_data(query, replacement_dict={}):
+    for k, v in replacement_dict.items():
+        query = query.replace("{" + k + "}", str(v))
+    return client.query(query).result().to_dataframe(bqstorage_client=bqstorageclient, progress_bar_type='tqdm')
+
+def main():
+
+    configs = [
+                    ['country_code'],
+                    ['country_code', 'device_category'],
+                    ['country_code', 'device_category', 'rtt_category'],
+                    ['country_code', 'domain'],
+                    ['country_code', 'domain', 'device_category']]#,
+                    #['country_code', 'domain', 'device_category', 'rtt_category']]
+
+    results_list = []
+    for c in configs:
+        dims = ', '.join(c)
+        print(f'doing dims: {dims}')
+
+        tablename_dims = '_'.join([c_[:3] for c_ in c])
+        tablename_dims_ad = tablename_dims + '_ad'
+        if 'rtt' in dims:
+            tablename_dims_ad = tablename_dims.replace('rtt', 'ad_rtt')
+
+        repl_dict = {'dims': dims,
+                     'tablename_dims': tablename_dims,
+                     'tablename_dims_ad': tablename_dims_ad}
+
+        query = open(os.path.join(sys.path[0], f"query_fs_ad_product_uplift.sql"), "r").read()
+        df = get_bq_data(query, repl_dict)
+        df['dims'] = dims
+        results_list.append(df)
+
+    results_df = pd.concat(results_list)
+
+    f = 0
+
+if __name__ == "__main__":
+    main()
+
---
+++ DAS_uplift/query_fs_ad_product_uplift.sql
@@ -0,0 +1,47 @@
+with config_ad as (
+    select date, {dims}, fs_ad_product, bidder
+    from `streamamp-qa-239417.DAS_2_0.DAS_config_geo_{tablename_dims_ad}_optimised_2025-07-11` t11
+    inner join unnest(t11.bidders) bidder
+    where array_length(bidders) = 10
+), perf_ad as (
+    select date, {dims}, fs_ad_product, avg(rps) rps_ad, avg(all_bidder_session_count) sessions_ad
+    from config_ad
+    inner join `streamamp-qa-239417.DAS_2_0.DAS_bidder_rps_geo_{tablename_dims_ad}_optimised_2025-07-11`
+    using (date, {dims}, fs_ad_product, bidder)
+    group by date, {dims}, fs_ad_product
+    having count(*) = 10
+), config_no_ad as (
+    select date, {dims}, bidder
+    from `streamamp-qa-239417.DAS_2_0.DAS_config_geo_{tablename_dims}_optimised_2025-07-11` t11
+    inner join unnest(t11.bidders) bidder
+    where array_length(bidders) = 10
+), perf_no_ad as (
+    select date, {dims}, fs_ad_product, avg(rps) rps_no_ad
+    from config_no_ad
+    inner join `streamamp-qa-239417.DAS_2_0.DAS_bidder_rps_geo_{tablename_dims_ad}_optimised_2025-07-11`
+    using (date, {dims}, bidder)
+    group by date, {dims}, fs_ad_product
+    having count(*) = 10
+), session_count_no_ad as (
+    select date, {dims}, avg(all_bidder_session_count) sessions_no_ad
+    from config_no_ad
+    inner join `streamamp-qa-239417.DAS_2_0.config_geo_{tablename_dims}_optimised_2025-07-11`
+    using (date, {dims}, bidder)
+    group by date, {dims}
+    having count(*) = 10
+), combined as (
+    select *, rps_ad/rps_no_ad-1 rps_uplift_ad
+    from perf_ad
+    inner join perf_no_ad
+    using (date, {dims}, fs_ad_product)
+    inner join session_count_no_ad
+    using (date, {dims})
+)
+select date,
+    count(*) cohorts,
+    sum(sessions_no_ad) sessions_no_ad,
+    sum(sessions_ad) sessions_ad,
+    sum(rps_uplift_ad * sessions_ad) / sum(sessions_ad) rps_uplift_ad_weighted
+from combined
+group by 1
+order by 1
