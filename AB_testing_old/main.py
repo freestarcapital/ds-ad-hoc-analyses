177de332a74fe610e5667098fccd0afc03bd1746
hughb <hughbarnes@freestar.io>
2025-09-05 13:27:08 +0100

    AB testing with prebid and GAM revenue

---
+++ AB_testing_old/main.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from google.cloud import bigquery
+import configparser
+from google.cloud import bigquery_storage
+import os, sys
+import datetime
+import pickle
+import scipy
+from matplotlib.backends.backend_pdf import PdfPages
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+config_path = '../config.ini'
+config = configparser.ConfigParser()
+config.read(config_path)
+
+project_id = "streamamp-qa-239417"
+client = bigquery.Client(project=project_id)
+bqstorageclient = bigquery_storage.BigQueryReadClient()
+
+def get_bq_data(query, replacement_dict={}):
+    for k, v in replacement_dict.items():
+        query = query.replace("{"+k+"}", f'{v}')
+    return client.query(query).result().to_dataframe(bqstorage_client=bqstorageclient, progress_bar_type='tqdm')
+
+def get_data(query_filename, data_cache_filename, force_requery=False, repl_dict = {}):
+    data_cache_filename_full = f'data_cache/{data_cache_filename}.pkl'
+
+    if not force_requery and os.path.exists(data_cache_filename_full):
+        print(f'found existing data file, loading {data_cache_filename_full}')
+        with open(data_cache_filename_full, 'rb') as f:
+            df = pickle.load(f)
+        return df
+
+    query = open(os.path.join(sys.path[0], f"queries/{query_filename}.sql"), "r").read()
+    df = get_bq_data(query, repl_dict)
+
+    with open(data_cache_filename_full, 'wb') as f:
+        pickle.dump(df, f)
+    return df
+
+
+def main():
+    # queries = open(os.path.join(sys.path[0], f"query_current.sql"), "r").read()
+    # get_bq_data(queries)
+
+    query = open(os.path.join(sys.path[0], f"query_with_pageview_raw.sql"), "r").read()
+    get_bq_data(query)
+
+
+if __name__ == "__main__":
+
+    main()
