72dae03ab8c27f3fabaf39bd136b759d6c935aa6
hughb <hughbarnes@freestar.io>
2025-09-18 15:41:11 +0100

    added formatting

--- AB_testing/main.py
+++ AB_testing/main.py
@@ -236,7 +236,7 @@ def main_process_csv():
     summary_uplift_mean, summary_uplift_error, summary_uplift_t_stats = create_table_summary(df_uplift, val_cols, True)
 
     writer = pd.ExcelWriter('results/test.xlsx', engine='xlsxwriter')
-    format_worksheet(writer, 'Summary of daily average values of uplift', summary_uplift_mean)
+    format_worksheet(writer, 'Summary of daily average change', summary_uplift_mean)
     # format_worksheet(writer, 'summary_uplift_error', summary_uplift_error)
     # format_worksheet(writer, 'summary_uplift_t_stats', summary_uplift_t_stats,'#,##0')
     format_worksheet(writer, 'Summary of daily average values', summary_mean, None)
